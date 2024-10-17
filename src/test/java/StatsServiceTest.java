97bce0cac975375096b3e1a8bbeb55a8388cd32b
Darya Jenkins <melissegreenleaf@gmail.com>
2024-10-18 01:42:35 +0200

    one mistake was found

--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -61,7 +61,7 @@ public class StatsServiceTest {
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         long expected = 5;
-        long actual = service.belowAverage(sales);
+        long actual = service.aboveAverage(sales);
 
         Assertions.assertEquals(expected, actual);
     }
